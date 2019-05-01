6e12739a320603858a3882e0e3f7b757f470602f
mmcgovern0 <mmcgovern.wework@gmail.com>
2019-05-01 10:43:15 -0400

    First Code Challenge in the BOOKS!!!

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,51 @@
 class Customer
-  attr_reader :first_name, :last_name
 
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name  = last_name
-  end
+  	attr_reader :first_name, :last_name
 
-  def full_name
-    "#{first_name} #{last_name}"
-  end
+  	@@all = []
+
+  	def initialize(first_name, last_name)
+    	@first_name = first_name
+    	@last_name  = last_name
+    	@@all << self
+  	end
+
+  	def self.all
+  		@@all
+  	end
+
+  	def full_name
+    	"#{first_name} #{last_name}"
+  	end
+
+  	def add_review(restaurant, content, rating)
+  		Review.new(self, restaurant, content, rating)
+  	end
+
+  	def num_reviews
+  		self.review.count
+  	end
+
+  	def restaurants
+  		restaurant = self.review.map(&:restaurant)
+  		restaurant.uniq
+  	end
+
+
+  	def self.find_by_name(name)
+  		@@all.find {|customer| customer.full_name == name}
+  	end
+
+  	def self.find_all_by_first_name(name)
+ 		@@all.find {|customer| customer.first_name == name}
+	end	
+
+	def self.all_names
+		@@all.map {|customer| customer.full_name}
+	end
+
+	def review
+ 		Review.all.select {|review| review.customer == self }
+	end
+
 end
