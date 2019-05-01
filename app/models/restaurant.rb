67bd89c378d78f0ec0f686369b9c6e299d3f8b66
Wei <theweifeng@gmail.com>
2019-05-01 10:42:38 -0400

    first commit

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,57 @@
 class Restaurant
   attr_reader :name
 
+  @@all = []
+  
   def initialize(name)
     @name = name
+    self.all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def all
+    self.class.all
+  end
+
+  def reviews
+    Review.all.select {|review| review.restaurant == self}
+  end
+
+  def customers
+    clients = self.reviews.map {|review| review.customer}
+    clients.uniq
+  end
+
+  def average_star_rating
+    ratings = self.reviews.map {|review| review.rating}
+    (ratings.sum * 1.0) / ratings.count
+  end
+
+  def longest_review
+    hash = {}
+    longest = nil
+    longest_length = nil
+
+    self.reviews.each {|review| hash[review] = review.content.length}
+
+    hash.each do |review, content_length|
+      if longest.nil? || content_length > longest_length
+        longest = review
+        longest_length = content_length
+      end
+    end
+
+    longest
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|restaurant| restaurant.name == name}
+  end
+
+  def inspect
+    @name
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,25 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+ted = Customer.new("Ted", "Mosby")
+barney = Customer.new("Barney", "Stinson")
+robin = Customer.new("Robin", "Scherbatsky")
+
+mc_larens = Restaurant.new("McLaren\'s Pub")
+hoser_hut = Restaurant.new("Hoser Hut")
+shinjitsu = Restaurant.new("Shinjitsu")
+
+ted.add_review(mc_larens, "fun fact about mclaren's.... blah blah", 5)
+barney.add_review(mc_larens, "great place to suit up", 5)
+robin.add_review(mc_larens, "meh", 4)
+
+
+robin.add_review(hoser_hut, "this is a real bar", 5)
+
+ted.add_review(shinjitsu, "fun fact about  shinjitsu .... blah blah", 5)
+barney.add_review(shinjitsu, "robin does not think i can leave a longer review than ted, challenge accepted! blah blah blah", 3)
+
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
